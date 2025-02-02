a4a805549beb7d5f6dfe7d2f37cd01bfebbb4c6c
Yevhenii <110053436+Yevhenii0536@users.noreply.github.com>
2025-02-02 19:35:29 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,35 @@
+from typing import Dict, Optional, List
+
+
 class Person:
-    # write your code here
-    pass
+    people: Dict[str, "Person"] = dict()
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
+
+    @staticmethod
+    def find_person(name: Optional[str]) -> Optional["Person"]:
+        return Person.people.get(name) if isinstance(name, str) else None
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: List[dict]) -> List[Person]:
+    Person.people.clear()
+
+    persons = [Person(name=p.get("name"), age=p.get("age")) for p in people]
+
+    for person in people:
+        current_person = Person.find_person(person.get("name"))
+
+        if current_person:
+            wife_name = person.get("wife")
+            husband_name = person.get("husband")
+
+            if isinstance(wife_name, str):
+                current_person.wife = Person.find_person(wife_name)
+
+            if isinstance(husband_name, str):
+                current_person.husband = Person.find_person(husband_name)
+
+    return persons
